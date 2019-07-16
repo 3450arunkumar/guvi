91f5f30ace4c8148d35577b1e0065f0fbfed478c
3450arunkumar <52955465+3450arunkumar@users.noreply.github.com>
2019-07-16 14:53:22 +0530

    Update Test-1.py

--- Test-1.py
+++ Test-1.py
@@ -1,5 +1,10 @@
-n=input('enter the number=')
+# positive case
+n=int(input('enter the number='))
 if n==3:
    print('positive')
 else:
    print('negative')
+# negative case
+n=int(input('enter the number='))
+if n==0:
+   print('zero')
